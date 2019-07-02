7ed6c39bd064cda475da0f53f25dfec7c76c52d7
Ben Houston <BenjaminAHouston@gmail.com>
2019-07-02 18:19:20 -0400

    Added console.log to help debug

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const auth = require('./auth/authRoutes');
 const user = require('./user/userRoutes');
 const log = require('../../config/middleware/console');
 
-router.use(log);
+// router.use(log);
 
 router.use('/auth', auth);
 router.use('/user', user);
